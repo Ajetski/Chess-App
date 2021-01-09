3bab34097e2216f3b70ce672b7d700bdd19a0c71
Ajetski <45019515+Ajetski@users.noreply.github.com>
2021-01-09 08:42:29 -0500

    refactor to functional component

--- src/components/Board/Board.js
+++ src/components/Board/Board.js
@@ -1,43 +1,42 @@
-import React from 'react';
+import { useState, useEffect } from 'react';
 import { Chessground } from 'chessground';
 import { toDests, playOtherSide } from '../../utils';
 import './styles/chessground.css';
 import './styles/theme.css';
 const Chess = require('chess.js');
 
-export default class Board extends React.Component {
-	constructor(props) {
-		super(props);
-		this.chess = new Chess();
-	}
+export default function Board(props) {
 
-	render() {
-		return (
-			<div ref={el => this.boardRef = el}
-				style={{
-					width: this.props.width,
-					height: this.props.height
-				}}>
-			</div>
-		);
-	}
+	const [chess] = useState(new Chess());
+	const [boardRef, setBoardRef] = useState();
 
-	componentDidMount() {
-		this.cg = Chessground(this.boardRef, {
-			movable: {
-				color: 'white',
-				free: false,
-				dests: toDests(this.chess),
-			},
-			draggable: {
-				showGhost: true
-			}
-		});
-		this.cg.set({
-			movable: { events: { after: playOtherSide(this.cg, this.chess) } }
-		});
-		document.getElementsByClassName('cg-wrap').item(0).classList.add('blue2');
-	}
+	useEffect(() => {
+		if (boardRef) {
+			const api = Chessground(boardRef, {
+				movable: {
+					color: 'white',
+					free: false,
+					dests: toDests(chess),
+				},
+				draggable: {
+					showGhost: true
+				}
+			})
+			api.set({
+				movable: { events: { after: playOtherSide(api, chess) } }
+			});
+			document.getElementsByClassName('cg-wrap').item(0).classList.add('blue2');
+		}
+	}, [boardRef, chess]);
+
+	return (
+		<div ref={el => setBoardRef(() => el)}
+			style={{
+				width: props.width,
+				height: props.height
+			}}>
+		</div>
+	);
 }
 
 Board.defaultProps = {
