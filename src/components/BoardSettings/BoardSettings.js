d36b3d1485edb7d58043b0df49da05c0a97c545c
Ajetski <45019515+Ajetski@users.noreply.github.com>
2021-01-09 05:21:16 -0500

    refactor class to funcitonal component

--- src/components/BoardSettings/BoardSettings.js
+++ src/components/BoardSettings/BoardSettings.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import { useState } from 'react';
 import './BoardSettings.css'
 
 function ListItem(props) {
@@ -15,66 +15,57 @@ function ListItem(props) {
 	);
 }
 
-export default class BoardSettings extends React.Component {
-	constructor(props) {
-		super(props);
-		this.state = {
-			theme: 'blue2'
-		};
-		this.changeTheme = this.changeTheme.bind(this);
-	}
+export default function BoardSettings() {
+	const [theme, setTheme] = useState('blue2')
 
-	changeTheme(e) {
+	const changeTheme = (e) => {
 		const boards = document.getElementsByClassName('cg-wrap');
-		const theme = e.currentTarget.dataset.id;
+		const newTheme = e.currentTarget.dataset.id;
 		for (let i = 0; i < boards.length; i++) {
 			const board = boards.item(i);
-			board.classList.remove(this.state.theme);
-			board.classList.add(theme);
+			board.classList.remove(theme);
+			board.classList.add(newTheme);
 		}
-		this.setState(() => ({ theme }));
+		setTheme(() => ({ newTheme }));
 	}
 
-	render() {
-		return (
-			< div className='settingsBox m-3'>
-				<button type="button" className="btn btn-primary" data-bs-toggle="modal" data-bs-target="#settingsModal">
-					Settings
+	return (
+		<div className='settingsBox m-3'>
+			<button type="button" className="btn btn-primary" data-bs-toggle="modal" data-bs-target="#settingsModal">
+				Settings
 				</button>
-				<div className="modal" tabIndex="-1" id="settingsModal">
-					<div className="modal-dialog">
-						<div className="modal-content">
-							<div className="modal-header">
-								<h5 className="modal-title">Board Settings</h5>
-							</div>
-							<div className="modal-body">
-								<p>Select a board:</p>
-								<ul>
-									<ListItem name="Blue"
-										id="blue"
-										handleClick={this.changeTheme}
-										currentTheme={this.state.theme}
-									/>
-									<ListItem name="Blue 2"
-										id="blue2"
-										handleClick={this.changeTheme}
-										currentTheme={this.state.theme}
-									/>
-									<ListItem name="Purple Diag"
-										id="purple-diag"
-										handleClick={this.changeTheme}
-										currentTheme={this.state.theme}
-									/>
-								</ul>
-								{/* <button type="button" className="float-right btn btn-danger" data-bs-dismiss="modal">Close</button> */}
-							</div>
-							<div className="modal-footer">
-								<button type="button" className="btn btn-danger" data-bs-dismiss="modal">Close</button>
-							</div>
+			<div className="modal" tabIndex="-1" id="settingsModal">
+				<div className="modal-dialog">
+					<div className="modal-content">
+						<div className="modal-header">
+							<h5 className="modal-title">Board Settings</h5>
+						</div>
+						<div className="modal-body">
+							<p>Select a board:</p>
+							<ul>
+								<ListItem name="Blue"
+									id="blue"
+									handleClick={changeTheme}
+									currentTheme={theme}
+								/>
+								<ListItem name="Blue 2"
+									id="blue2"
+									handleClick={changeTheme}
+									currentTheme={theme}
+								/>
+								<ListItem name="Purple Diag"
+									id="purple-diag"
+									handleClick={changeTheme}
+									currentTheme={theme}
+								/>
+							</ul>
+						</div>
+						<div className="modal-footer">
+							<button type="button" className="btn btn-danger" data-bs-dismiss="modal">Close</button>
 						</div>
 					</div>
 				</div>
-			</ div >
-		);
-	}
+			</div>
+		</ div >
+	);
 }
