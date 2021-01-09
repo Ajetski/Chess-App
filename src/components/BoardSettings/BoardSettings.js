281dba549c7c0d7a46aabf449e5bd993412b80d8
Ajetski <45019515+Ajetski@users.noreply.github.com>
2021-01-09 05:49:52 -0500

    refactor to use modal component

--- src/components/BoardSettings/BoardSettings.js
+++ src/components/BoardSettings/BoardSettings.js
@@ -1,7 +1,8 @@
 import { useState } from 'react';
+import Modal, { ModalBody, ModalFooter, ModalHeader, ModalTitle } from '../Modal/Modal';
 import './BoardSettings.css'
 
-function ListItem(props) {
+function BoardTheme(props) {
 	const active = props.currentTheme === props.id;
 	return (
 		<li onClick={props.handleClick} data-id={props.id}>
@@ -23,49 +24,46 @@ export default function BoardSettings() {
 		const newTheme = e.currentTarget.dataset.id;
 		for (let i = 0; i < boards.length; i++) {
 			const board = boards.item(i);
+			console.log(board.classList);
 			board.classList.remove(theme);
 			board.classList.add(newTheme);
 		}
-		setTheme(() => ({ newTheme }));
+		setTheme(() => newTheme);
 	}
 
 	return (
 		<div className='settingsBox m-3'>
 			<button type="button" className="btn btn-primary" data-bs-toggle="modal" data-bs-target="#settingsModal">
 				Settings
-				</button>
-			<div className="modal" tabIndex="-1" id="settingsModal">
-				<div className="modal-dialog">
-					<div className="modal-content">
-						<div className="modal-header">
-							<h5 className="modal-title">Board Settings</h5>
-						</div>
-						<div className="modal-body">
-							<p>Select a board:</p>
-							<ul>
-								<ListItem name="Blue"
-									id="blue"
-									handleClick={changeTheme}
-									currentTheme={theme}
-								/>
-								<ListItem name="Blue 2"
-									id="blue2"
-									handleClick={changeTheme}
-									currentTheme={theme}
-								/>
-								<ListItem name="Purple Diag"
-									id="purple-diag"
-									handleClick={changeTheme}
-									currentTheme={theme}
-								/>
-							</ul>
-						</div>
-						<div className="modal-footer">
-							<button type="button" className="btn btn-danger" data-bs-dismiss="modal">Close</button>
-						</div>
-					</div>
-				</div>
-			</div>
-		</ div >
+			</button>
+			<Modal id="settingsModal">
+				<ModalHeader>
+					<ModalTitle>Board Settings</ModalTitle>
+				</ModalHeader>
+				<ModalBody>
+					<p>Select a board:</p>
+					<ul>
+						<BoardTheme name="Blue"
+							id="blue"
+							handleClick={changeTheme}
+							currentTheme={theme}
+						/>
+						<BoardTheme name="Blue 2"
+							id="blue2"
+							handleClick={changeTheme}
+							currentTheme={theme}
+						/>
+						<BoardTheme name="Purple Diag"
+							id="purple-diag"
+							handleClick={changeTheme}
+							currentTheme={theme}
+						/>
+					</ul>
+				</ModalBody>
+				<ModalFooter>
+					<button type="button" className="btn btn-danger" data-bs-dismiss="modal">Close</button>
+				</ModalFooter>
+			</Modal>
+		</div>
 	);
 }
