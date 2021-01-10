3eec312845bdfeb961c48eed5962126cbbd689d5
Ajetski <45019515+Ajetski@users.noreply.github.com>
2021-01-10 09:36:19 -0500

    fix cols and add game history

--- src/components/App.js
+++ src/components/App.js
@@ -1,26 +1,24 @@
 import Board from './Board';
 import BoardSettings from './BoardSettings';
+import GameHistory from './GameHistory';
 
 export default function App() {
 	return (
-		<>
-			<div>
+		<div className="container">
+			<div className="row">
 				<p>some header</p>
 			</div>
-			<div className="fluid-container">
-				<div className="col-2">
-
-				</div>
-				<div className="col-8 mx-auto">
+			<div className="row">
+				<div className="col-8">
 					<div className="mx-auto" style={{ width: 'fit-content' }}>
 						<Board />
 						<BoardSettings />
 					</div>
 				</div>
-				<div className="col-2">
-
+				<div className="col-4">
+					<GameHistory />
 				</div>
 			</div>
-		</>
+		</div>
 	);
 }
