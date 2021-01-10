54b5d6cf1a8a7735d1695a05acc89f96b7f0e63b
Ajetski <45019515+Ajetski@users.noreply.github.com>
2021-01-10 09:35:37 -0500

    add redux to app

--- src/index.js
+++ src/index.js
@@ -1,11 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App';
+import configureStore from './store/configureStore';
+import { Provider } from 'react-redux';
 import './index.css'
+
+const store = configureStore();
+
+// Log updates to store
+// store.subscribe(state => {
+// 	console.log('Redux state updated: ', state);
+// });
 
 ReactDOM.render(
 	<React.StrictMode>
-		<App />
+		<Provider store={store}>
+			<App />
+		</Provider>
 	</React.StrictMode>,
 	document.getElementById('root')
 );
