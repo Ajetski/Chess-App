6991255279017044dad0425db2dc271d503c03e7
Ajetski <45019515+Ajetski@users.noreply.github.com>
2021-01-10 19:20:43 -0500

    add engine reducer and dispatch default state

--- src/store/configureStore.js
+++ src/store/configureStore.js
@@ -1,7 +1,31 @@
-import { createStore } from 'redux';
-import chessReducer from "../reducers/chessReducer";
+import { createStore, combineReducers } from 'redux';
+import Chess from 'chess.js';
+import chessReducer from '../reducers/chessReducer';
+import engineReducer from '../reducers/engineReducer';
+import { setMaxDepth, updateEval, setEngine } from '../actions/engineActions';
+import setChess from '../actions/chessActions';
 
 export default function configureStore() {
-	const store = createStore(chessReducer);
+	const store = createStore(
+		combineReducers({ chess: chessReducer, engine: engineReducer })
+	);
+
+	store.dispatch(updateEval({
+		evaluation: 0.00,
+		bestmove: '',
+		line: [],
+		depth: 1
+	}));
+
+	store.dispatch(setEngine({
+		engine: new Worker('stockfish.js')
+	}));
+
+	store.dispatch(setMaxDepth({
+		maxDepth: 25
+	}));
+
+	store.dispatch(setChess({ chess: new Chess() }));
+
 	return store;
 }
