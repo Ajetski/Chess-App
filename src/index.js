3847be1d01f3b33d97e640aa8e8e62729e0d7112
Ajetski <45019515+Ajetski@users.noreply.github.com>
2021-01-11 04:01:49 -0500

    set material ui theme

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
+import { ThemeProvider, createMuiTheme } from '@material-ui/core';
 import App from './components/App';
 import configureStore from './store/configureStore';
 import './index.css'
@@ -12,10 +13,18 @@ const store = configureStore();
 // 	console.log('Redux state updated: ', state);
 // });
 
+const theme = createMuiTheme({
+	palette: {
+		type: 'dark'
+	}
+});
+
 ReactDOM.render(
 	<React.StrictMode>
 		<Provider store={store}>
-			<App />
+			<ThemeProvider theme={theme}>
+				<App />
+			</ThemeProvider>
 		</Provider>
 	</React.StrictMode>,
 	document.getElementById('root')
