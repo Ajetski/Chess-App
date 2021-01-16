35e5af5e3cd8bdc5c25c529cb69ee544b4c22ab4
Ajetski <45019515+Ajetski@users.noreply.github.com>
2021-01-15 20:03:23 -0500

    update code style for imports

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { ThemeProvider, createMuiTheme } from '@material-ui/core';
+
 import App from './components/App';
 import configureStore from './store/configureStore';
 import './index.css'
