39d8b967c6f168a724aa9bc159f77704790cade3
Ajetski <45019515+Ajetski@users.noreply.github.com>
2021-01-17 15:50:35 -0500

    init navbar

---
+++ src/components/Navbar.js
@@ -0,0 +1,25 @@
+import { Link } from "react-router-dom";
+
+const Navbar = () => (
+	<>
+		<ul className="nav justify-content-center">
+			<li className="nav-item">
+				<Link className="nav-link" to="/">
+					<button className="btn btn-primary">
+						Home
+					</button>
+				</Link>
+			</li>
+			<li className="nav-item">
+				<Link className="nav-link" to="/analysis">
+					<button className="btn btn-primary">
+						Analysis
+					</button>
+				</Link>
+			</li>
+		</ul>
+	</>
+
+);
+
+export default Navbar;
