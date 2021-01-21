c7ca9dd982500638080f08bf27117e1014e9176a
Ajetski <45019515+Ajetski@users.noreply.github.com>
2021-01-20 19:45:29 -0500

    change to arrow syntax; avoid using return

--- src/components/App.js
+++ src/components/App.js
@@ -9,37 +9,37 @@ import Home from './Home';
 import Game from './Game';
 import Navbar from './Navbar';
 
-export default function App() {
-	return (
-		<div className="container">
-			<Router>
-				<div className="row mb-2">
-					<h3 className="my-auto mr-3">Chess App</h3>
-					<Navbar />
-				</div>
-				<div className="row">
-					<Switch>
-						<Route path="/" exact>
-							<Home />
-						</Route>
-						<Route path="/analysis">
-							<Game />
-						</Route>
-						<Route path="/game/:gameId">
-							<Game />
-						</Route>
-						<GuardedRoute auth={true} path="/allow">
-							<p>authenticated</p>
-						</GuardedRoute>
-						<GuardedRoute auth={false} path="/disallow">
-							<p>not authenticated</p>
-						</GuardedRoute>
-						<Route path="/">
-							<p>NOT FOUND</p>
-						</Route>
-					</Switch>
-				</div>
-			</Router>
-		</div>
-	);
-}
+const App = () => (
+	<div className="container">
+		<Router>
+			<div className="row mb-2">
+				<h3 className="my-auto mr-3">Chess App</h3>
+				<Navbar />
+			</div>
+			<div className="row">
+				<Switch>
+					<Route path="/" exact>
+						<Home />
+					</Route>
+					<Route path="/analysis">
+						<Game />
+					</Route>
+					<Route path="/game/:gameId">
+						<Game />
+					</Route>
+					<GuardedRoute auth={true} path="/allow">
+						<p>authenticated</p>
+					</GuardedRoute>
+					<GuardedRoute auth={false} path="/disallow">
+						<p>not authenticated</p>
+					</GuardedRoute>
+					<Route path="/">
+						<p>NOT FOUND</p>
+					</Route>
+				</Switch>
+			</div>
+		</Router>
+	</div>
+);
+
+export default App;
