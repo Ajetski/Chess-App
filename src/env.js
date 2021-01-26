588edcbcffbf8a734bb3edf5f9d3601ac81d2ac7
Ajetski <45019515+Ajetski@users.noreply.github.com>
2021-01-25 21:22:48 -0500

    update env

--- src/env.js
+++ src/env.js
@@ -1,6 +1,16 @@
-const env = {
-	apiUrl: 'ws://localhost:3001',
-	siteURL: 'http://localhost:3000'
-};
+const dev = false;
+let env;
+
+if (dev) { // for dev
+	env = {
+		apiUrl: 'ws://localhost:3001',
+		siteURL: 'http://localhost:3000'
+	};
+} else { // for prod
+	env = {
+		apiUrl: 'ws://ajet-chess-app-server.herokuapp.com',
+		siteURL: 'https://ajet-chess-app.herokuapp.com'
+	};
+}
 
 export default env;
