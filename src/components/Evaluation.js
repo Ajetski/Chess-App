bcf6dd0aa32963a7003fa3e7515df04943a4026e
Ajetski <45019515+Ajetski@users.noreply.github.com>
2021-01-26 19:41:12 -0500

    init eval component

---
+++ src/components/Evaluation.js
@@ -0,0 +1,40 @@
+/* eslint-disable react-hooks/exhaustive-deps */
+import { useState, useEffect } from 'react';
+import { connect } from 'react-redux';
+
+const Evaluation = ({ engine, chess }) => {
+	const [maxDepth, setMaxDepth] = useState(engine.maxDepth);
+
+	useEffect(() => {
+		setMaxDepth(() => engine.maxDepth);
+	}, [chess]);
+
+	useEffect(() => {
+		if (maxDepth !== engine.maxDepth)
+			engine.engine.postMessage(`go depth ${engine.depth + 5}`);
+	}, [maxDepth]);
+
+	return (
+		<div>
+			<div>{`Analysis: Depth ${engine.depth}/${maxDepth}`} {
+				engine.depth === maxDepth &&
+				<button
+					className="btn btn-info"
+					onClick={() => setMaxDepth(() => maxDepth + 5)}>
+					+
+				</button>
+			}
+			</div>
+			<div>Best Move: {engine.bestmove}</div>
+			<div>Evaluaiton: {engine.evaluation}</div>
+		</div>
+	);
+}
+
+const mapStateToProps = (state, ownProps) => ({
+	...ownProps,
+	chess: state.chess.chess,
+	engine: state.engine
+});
+
+export default connect(mapStateToProps)(Evaluation);
