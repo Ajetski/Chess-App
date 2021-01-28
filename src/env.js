dd80e5239ef2ee14305a55d603afb9d3fce6a736
Caleb Garrick <caleb.garrick@gmail.com>
2021-01-28 16:34:19 -0500

    #8 add environment vars for http
    Co-Authored-By: Adam Jeniski <45019515+Ajetski@users.noreply.github.com>

--- src/env.js
+++ src/env.js
@@ -3,12 +3,14 @@ let env;
 
 if (dev) { // for dev
 	env = {
-		apiUrl: 'ws://localhost:3001',
+		wsUrl: 'ws://localhost:3001',
+		apiUrl: 'http://localhost:3001',
 		siteURL: 'http://localhost:3000'
 	};
 } else { // for prod
 	env = {
-		apiUrl: 'wss://ajet-chess-app-server.herokuapp.com',
+		wsUrl: 'wss://ajet-chess-app-server.herokuapp.com',
+		apiUrl: 'https://ajet-chess-app-server.herokuapp.com',
 		siteURL: 'https://ajet-chess-app.herokuapp.com'
 	};
 }
