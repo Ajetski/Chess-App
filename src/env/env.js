74e361a006efb27347b47708afff09fa7a03e082
Ajetski <45019515+Ajetski@users.noreply.github.com>
2021-01-28 23:42:00 -0500

    #20 add env files for each deployment

---
+++ src/env/env.js
@@ -0,0 +1,13 @@
+const env = {
+	wsUrl: 'ws://localhost:3001',
+	apiUrl: 'http://localhost:3001',
+	siteURL: 'http://localhost:3000'
+};
+
+export default env;
+
+// env = {
+// 	wsUrl: 'wss://ajet-chess-app-server.herokuapp.com',
+// 	apiUrl: 'https://ajet-chess-app-server.herokuapp.com',
+// 	siteURL: 'https://ajet-chess-app.herokuapp.com'
+// };
