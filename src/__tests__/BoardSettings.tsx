cd7c0a7b473a44b84f2e72f8eafd04f1178b847b
Ajetski <45019515+Ajetski@users.noreply.github.com>
2021-02-06 19:45:33 -0500

    #43 add redux store to components with modals

--- src/__tests__/BoardSettings.tsx
+++ src/__tests__/BoardSettings.tsx
@@ -1,8 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import 'jsdom-worker';
+
 import BoardSettings from '../components/BoardSettings';
+import configureStore from '../store/configureStore';
+
+const store = configureStore();
+
 
 it('renders without crashing', () => {
 	const div = document.createElement('div');
-	ReactDOM.render(<BoardSettings />, div);
+	ReactDOM.render(<Provider store={store}>
+		<BoardSettings />
+	</Provider>, div);
 });
--- src/__tests__/Home.tsx
+++ src/__tests__/Home.tsx
@@ -1,8 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import 'jsdom-worker';
+
 import Home from '../components/Home';
+import configureStore from '../store/configureStore';
+
+const store = configureStore();
 
 it('renders without crashing', () => {
     const div = document.createElement('div');
-    ReactDOM.render(<Home />, div);
+    ReactDOM.render(<Provider store={store}>
+        <Home />
+    </Provider>, div);
 });
