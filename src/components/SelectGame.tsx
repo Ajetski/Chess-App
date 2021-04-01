09465a6e29103ae276ebc53b90ae629cd643f61f
Caleb Garrick <caleb.garrick@gmail.com>
2021-04-01 19:32:31 -0400

    #49 add react-bootstrap to SelectGame, remove material-ui

--- src/components/SelectGame.tsx
+++ src/components/SelectGame.tsx
@@ -1,13 +1,8 @@
 import { FC, useEffect, useState } from 'react';
 import { useHistory } from 'react-router-dom';
 import axios from 'axios';
-import Table from '@material-ui/core/Table';
-import TableBody from '@material-ui/core/TableBody';
-import TableCell from '@material-ui/core/TableCell';
-import TableContainer from '@material-ui/core/TableContainer';
-import TableHead from '@material-ui/core/TableHead';
-import TableRow from '@material-ui/core/TableRow';
-import Paper from '@material-ui/core/Paper';
+import Table from 'react-bootstrap/Table';
+import Button from 'react-bootstrap/Button';
 
 import env from '../env/env';
 
@@ -25,31 +20,29 @@ const SelectGame: FC = () => {
     }, [])
 
     return (
-        <TableContainer component={Paper}>
-            <Table aria-label="simple table">
-                <TableHead>
-                    <TableRow>
-                        <TableCell align="left">Game Id</TableCell>
-                        <TableCell align="left">Num Moves</TableCell>
-                        <TableCell></TableCell>
-                    </TableRow>
-                </TableHead>
-                <TableBody>
-                    {rows.map((row) => (
-                        <TableRow key={row.id}>
-                            <TableCell align="left">{row.id}</TableCell>
-                            <TableCell align="left">{row.numMoves}</TableCell>
-                            <TableCell align="left">
-                                <button className="btn btn-primary"
-                                    onClick={() => history.push(`game/watch/${row.id}`)}>
-                                    Spectate
-                                </button>
-                            </TableCell>
-                        </TableRow>
-                    ))}
-                </TableBody>
-            </Table>
-        </TableContainer >
+        <Table striped bordered hover variant="dark" aria-label="simple table">
+            <thead>
+                <tr>
+                    <th align="left">Game Id</th>
+                    <th align="left">Num Moves</th>
+                    <th></th>
+                </tr>
+            </thead>
+            <tbody>
+                {rows.map((row) => (
+                    <tr key={row.id}>
+                        <td align="left">{row.id}</td>
+                        <td align="left">{row.numMoves}</td>
+                        <td align="left">
+                            <Button variant="primary"
+                                onClick={() => history.push(`game/watch/${row.id}`)}>
+                                Spectate
+                            </Button>
+                        </td>
+                    </tr>
+                ))}
+            </tbody>
+        </Table>
     );
 }
 
