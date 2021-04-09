8ea64cec068c5a566cec49cd984b3fecbefb24f7
Ajetski <45019515+Ajetski@users.noreply.github.com>
2021-04-08 23:08:44 -0400

    add hooks for chess games

---
+++ src/hooks/useChess.tsx
@@ -0,0 +1,75 @@
+import { ChessInstance, ShortMove } from 'chess.js';
+import { useEffect, useState } from 'react';
+
+import { newChess, pgnToChess } from '../utils';
+
+export type ChessValues = {
+	chess: ChessInstance,
+	orientation: 'white' | 'black',
+}
+
+export type UpdateChess = {
+	move: (m: string | ShortMove) => void,
+	undo: VoidFunction,
+	reset: VoidFunction,
+	setPgn: (pgn: string) => void,
+	flip: VoidFunction
+};
+
+
+export const useChess = (pgn?: string): [ ChessValues, UpdateChess ] => {
+	const [chess, setChess] = useState(newChess(pgn));
+	const [orientation, setOrientation] = useState<'white' | 'black'>('white')
+
+	useEffect(() => {
+		console.log(chess.pgn());
+	}, [chess])
+
+	return [{ chess, orientation }, {
+		move: m => {
+			chess.move(m);
+			setChess(pgnToChess(chess.pgn()));
+		},
+		undo: () => {
+			chess.undo();
+			setChess(pgnToChess(chess.pgn()));
+		},
+		reset: () => {
+			chess.reset();
+			setChess(pgnToChess(chess.pgn()));
+		},
+		setPgn: pgn => {
+			setChess(pgnToChess(pgn));
+		},
+		flip: () => {
+			setOrientation(orientation === 'white' ? 'black' : 'white');
+		}
+	}];
+}
+
+/*
+if (engine) {
+			engine.postMessage('stop');
+			engine.postMessage(`position fen ${chess.fen()}`);
+			engine.postMessage(`go depth ${maxDepth}`);
+
+			// engine.postMessage(`go movetime 5000`);
+
+			engine.onmessage = function (event) {
+				const data = (event.data ? event.data : event).split(' ');
+				if (data[0] === 'info' && setEval) {
+					const startLineIdx = data.indexOf('pv') + 1;
+					const startEvalIdx = data.indexOf('score') + 1;
+					const colorMultiplier = chess.turn() === 'w' ? 1 : -1;
+					setEval({
+						depth: parseInt(data[2]),
+						evaluation: data[startEvalIdx] === 'cp'
+							? `${colorMultiplier * data[startEvalIdx + 1] / 100}` as string
+							: `#${colorMultiplier * data[startEvalIdx + 1]}` as string,
+						bestmove: data[startLineIdx],
+						line: data.slice(startLineIdx, data.length - 3),
+					});
+				}
+			};
+		}
+		*/
---
+++ src/hooks/useOnlineChess.tsx
@@ -0,0 +1,63 @@
+import { useState, useEffect } from 'react';
+
+import { ChessValues, useChess } from './useChess';
+import { connectToGame, makeMove, stayConnected } from '../ws/actions/gameActions';
+import env from '../env/env';
+import { ShortMove } from 'chess.js';
+
+export type UpdateOnlineChess = {
+	move: (m: string | ShortMove) => void,
+};
+
+export const useOnlineChess = (id: number): [ChessValues, UpdateOnlineChess] => {
+	const [{ chess, orientation }, updateChess] = useChess();
+
+	const [ws] = useState(new WebSocket(env.wsUrl));
+
+	useEffect(() => {
+		if (id) {
+			ws.onopen = async () => {
+				ws.send(connectToGame({
+					id,
+					isPlayer: true
+				}));
+				while (ws.readyState === 1) {
+					ws.send(stayConnected());
+					await new Promise<void>(resolve =>
+						setTimeout(() => resolve(), 15000));
+				}
+			}
+			ws.onmessage = ({ data }) => {
+				const action = JSON.parse(data);
+				if (action.type === 'game/connect') {
+					if (action.orientation === orientation)
+						updateChess.flip();
+					updateChess.setPgn(action.pgn);
+				} else if (action.type === 'game/move') {
+					updateChess.setPgn(action.pgn);
+				}
+			};
+		}
+		return () => {
+			ws.close();
+		}
+	}, [ws]);
+
+	return [{ chess, orientation }, {
+		move: m => {
+			if (ws.readyState === 1) {
+				updateChess.move(m);
+				ws.send(makeMove({ id, move: chess.history().splice(-1)[0] }));
+			}
+		},
+		// offerTakeback: () => {
+
+		// },
+		// offerDraw: () => {
+
+		// },
+		// resign: () => {
+
+		// }
+	}]
+};
